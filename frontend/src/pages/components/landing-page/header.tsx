3df95080a5ff307533559fbfd4d463991b1b73f7
xKuraido <81272935+KuraidoXenpaI@users.noreply.github.com>
2023-11-19 02:53:08 +0800

    added nextjs framework with react and tailwind

---
+++ frontend/src/pages/components/landing-page/header.tsx
@@ -0,0 +1,19 @@
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <h1>Logo</h1>
+            </div>
+
+            <div className="">
+                <span>Home</span>
+                <span>Community</span>
+                <span>Careers</span>
+                <span>About us</span>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
